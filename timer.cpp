7fbf141aa46cfc038821c972a473e438b9280711
bestitachi <qiji94@163.com>
2019-05-21 20:03:18 +0800

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -13,7 +13,22 @@ string tm2str(const struct tm &st, const string &format="%Y%m%d")
 
         return string(sTimeString);
 }
-
+int str2tm(const string &str, const string &format, struct tm &st)
+{
+        char *p = strptime(str.c_str(), format.c_str(), &st);
+        return (p != NULL) ? 0 : -1;
+}
+time_t str2timet(const string &str, const string &fomat){
+        struct tm st;
+        memset(&st,0,sizeof(st));
+        char *p = strptime(str.c_str(), fomat.c_str(), &st);
+        int ret=  (p != NULL) ? 0 : -1;
+        if(ret == 0)
+        {
+            return mktime(&st);
+        }
+        return ret;
+}
 string tm2str(const time_t &t, const string &format)
 {
     struct tm tt;
@@ -28,4 +43,11 @@ string FormateUnixtime(const string & timet)
     time_t t = stol(timet);
     //s << put_time(std::localtime(&t),"%F %T");//c++11
     return tm2str(t,"%F %T");
+}
+
+string DaysAgo(const string & timet,const string & format="%Y%m%d",int n=1)
+{
+    auto unix_t = str2timet(timet,format);
+    unix_t -= 86400*n;
+    return tm2str(unix_t,format);
 }
